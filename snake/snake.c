83cbba06ba8fadea00b879a3169fddcbe4d46363
Tomas Bandza <tomas.bandza@telia.lt>
2020-03-31 16:30:04 +0300

    upddates

--- snake/snake.c
+++ snake/snake.c
@@ -2,10 +2,12 @@
 #include <wchar.h>
 #include <locale.h>
 
-#define N 20
-#define M 40
+#define N 20 
+#define M 60
 
+#define CCHAR '@'
 #define BCHAR '*'
+#define SCHAR ' '
 
 /*
 void printAscii()
@@ -23,14 +25,42 @@ void printAscii()
 */
 int main()
 {
+ int i,j; 
+ for(i=0; i<=M+1; i++){
+   if (i==0){
+     printf("%c", CCHAR);
+   } else if (i==M+1) {
+     printf("%c", CCHAR);
+   } else {
+      printf("%c", BCHAR);
+   }
+ }
+ 
+ printf("\n");
+ 
+ for(i=0; i<N; i++){
+    printf("%c", BCHAR);
+    for(j=1; j<=M+1 ; j++) {
+      if (j==M+1) {
+        printf("%c\n", BCHAR);
+      } else {
+        printf("%c", SCHAR); 
+      }  
+    }
+ } 
+   
+// printf("\n");
 
- for(i=0; i<M+1; i++) 
- {
-   printf("border sim: %c", BCHAR);
-
+ for(i=0; i<=M+1; i++){
+   if (i==0){
+     printf("%c", CCHAR);
+   } else if (i==M+1) {
+     printf("%c", CCHAR);
+   } else {
+      printf("%c", BCHAR);
+   }
  }
 
- printf("border sim: %c", BCHAR);
 
  printf("\n");
  return 0;
