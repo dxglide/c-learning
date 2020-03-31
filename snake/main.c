7bfee699657b2dc9e94b992b49eafad6eb3acbeb
Tomas Bandza <tomas.bandza@telia.lt>
2020-03-31 08:17:12 +0300

    Snake game? :)

---
+++ snake/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/* Main  :) */
+int main()
+{
+
+  printf("===== Snake ========== \n");
+  return 0;
+}
+
+
+
---
+++ tests/tests.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/* Main  :) */
+int main()
+{
+
+  printf("===== Check: operatros \n");  
+  int  var = 4;
+  if  (var == 4)
+     printf("var is 4\n");
+  else
+     printf("var is not 4"); 
+
+  printf("===== Check: casting \n");
+  
+  char c = 'b';
+  printf("ASCII for value  'b' is %d. \n",  (int)c);  
+ 
+  return 0;
+}
+
+
+
