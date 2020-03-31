deb6916829314297c5ca1537fd984c74ba73c79a
Tomas Bandza <tomas.bandza@telia.lt>
2020-03-31 10:08:59 +0300

    tests

--- snake/snake.c
+++ snake/snake.c
@@ -1,10 +1,40 @@
 #include <stdio.h>
-/* Main  :) */
+#include <wchar.h>
+#include <locale.h>
+
+#define N 20
+#define M 40
+
+#define BCHAR '*'
+
+/*
+void printAscii()
+{
+  unsigned char count;
+    for(count=32; count< 255; count+=1)
+    {
+        printf("  %3d - %c",count,count);
+        if(count % 6==0)
+            printf("\n");
+    }
+    printf("\n");
+
+}
+*/
 int main()
 {
 
-  printf("===== Snake ========== \n");
-  return 0;
+ for(i=0; i<M+1; i++) 
+ {
+   printf("border sim: %c", BCHAR);
+
+ }
+
+ printf("border sim: %c", BCHAR);
+
+ printf("\n");
+ return 0;
+
 }
 
 
