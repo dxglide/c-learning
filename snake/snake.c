9c716ebc73bb6532d3c611239587fb8689a15732
Tomas Bandza <tomas.bandza@telia.lt>
2020-04-03 15:22:01 +0300

    Standard keyboard catch

--- snake/snake.c
+++ snake/snake.c
@@ -1,14 +1,12 @@
-//#include <stdio.h>
-//#include <wchar.h>
-//#include <locale.h>
-
-//#include <iostream>   // Standard Header
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/ioctl.h>
-#include <termios.h>
-#include <stropts.h>
 #include <stdio.h>
+#include <time.h>
+#include <math.h>
+//Linux Libraries
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+
 
 
 #define N 20
@@ -18,10 +16,94 @@
 #define BCHAR '*'
 #define SCHAR ' '
 
+// set cursor position  by current terminal window from upper left conner
+//
+//Linux Functions - These functions emulate some functions from the windows only conio header file
+//Code: http://ubuntuforums.org/showthread.php?t=549023
+
+
+void gotoxy(int x,int y)
+{
+   printf("%c[%d;%df",0x1B,y,x);
+}
+
+
+//http://cboard.cprogramming.com/c-programming/63166-kbhit-linux.html
+int kbhit(void)
+	{
+	  struct termios oldt, newt;
+	  int ch;
+	  int oldf;
+
+	  tcgetattr(STDIN_FILENO, &oldt);
+	  newt = oldt;
+	  newt.c_lflag &= ~(ICANON | ECHO);
+	  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+	  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+	  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+	  ch = getchar();
+
+	  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+	  fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+	  if(ch != EOF)
+	  {
+		ungetc(ch, stdin);
+		return 1;
+	  }
+
+	  return 0;
+}
+
+
+char getch()
+{
+   char c;
+   system("stty raw");
+   c= getchar();
+   system("stty sane");
+   printf("%c",c);
+   return(c);
+}
+
+void clrscr()
+{
+	system("clear");
+	return;
+}
+
+
+char waitForAnyKey(void)
+{
+	int pressed;
+
+	while(!kbhit());
+
+	pressed = getch();
+	//pressed = tolower(pressed);
+	return((char)pressed);
+}
+
 
 int main()
 {
+ char pressed;
+ printf("Press any key ... \n");
+ 
+ do
+ { 
+     pressed = waitForAnyKey();
+     pressed = tolower(pressed);
+ } while (!(pressed == 'y' || pressed == 'n'));
 
+ if (pressed == 'y')
+ {
+    clrscr(); //clear the console
+    exit(1);
+ }
+
+ //gotoxy(1,2);
 
  printf("\n");
  return 0;
